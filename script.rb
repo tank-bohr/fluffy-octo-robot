a6c323c3f6d4adf9243a7d03c31cb58906f18196
Alexey Nikitin <nikitin@bookmate.com>
2017-04-24 20:51:24 +0300

    init

---
+++ script.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'ruby-progressbar'
+require 'mechanize'
+require 'net/http'
+require 'net/https'
+require 'pry'
+
+PAGE_URL = 'https://book-audio.com/find'.freeze
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) " \
+    "AppleWebKit/537.36 (KHTML, like Gecko) " \
+    "Chrome/57.0.2987.133 Safari/537.36".freeze
+
+PARAMS = {
+  sort: :downloads,
+  order: :desc,
+  excludeRead: 0,
+  withAudio: 0,
+  duration: :any,
+  search: ''.freeze,
+  extendedList: :find,
+}.freeze
+
+PAGES_COUNT = 1072
+
+agent = Mechanize.new { |a| a.user_agent = USER_AGENT }
+
+page = agent.get(PAGE_URL)
+scripts = page.xpath('//script[not(@*)]')
+md = Enumerator.new do |matches|
+  scripts.each { |node| matches << node.text.match(/window.CSRF\s=\s\'(?<csrf>[\d\w]+)\'\;/) }
+end.detect { |md| !md.nil? }
+csrf = md[:csrf]
+
+base_params = PARAMS.merge(CSRF: csrf)
+output = File.open('output.json', 'w')
+progress_bar = ProgressBar.create(total: PAGES_COUNT, title: "I'm working...")
+
+(1..PAGES_COUNT).each do |page|
+  uri = URI('https://book-audio.com/find/allcards'.freeze)
+  params = base_params.merge(page: page)
+  uri.query = URI.encode_www_form(params)
+  request = Net::HTTP::Get.new(uri)
+  request['User-Agent'.freeze] = USER_AGENT
+  request['X-Requested-With'.freeze] = 'XMLHttpRequest'.freeze
+  result =  Net::HTTP.start(uri.hostname, uri.port, use_ssl: 'https'.freeze == uri.scheme) do |http|
+    http.request(request)
+  end
+  progress_bar.increment
+  output.puts(result.body)
+end
+
+output.close
+
+puts 'Done!'
